52e3fad281b8b12b67e37037906bdf0dbce999a4
codewizz <c.chaiwat.ch@gmail.com>
2021-04-12 16:46:24 +0700

    add: product serializer
    Signed-off-by: codewizz <c.chaiwat.ch@gmail.com>

---
+++ djackets_django/product/serializers.py
@@ -0,0 +1,17 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Category, Product
+
+
+class ProductSerializer(serializers.ManyRelatedField):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail",
+        )
