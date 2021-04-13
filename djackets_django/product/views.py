e730dea0b944fa36ebe43dbcda0b5677db763a10
codewizz <c.chaiwat.ch@gmail.com>
2021-04-13 12:19:30 +0700

    add: get product detail

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -20,3 +20,8 @@ class ProductDetail(APIView):
             return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
         except Product.DoesNotExist:
             raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
