1a626893c0a727020638da388186ab53ba5b5370
codewizz <c.chaiwat.ch@gmail.com>
2021-04-13 17:10:24 +0700

    add: vue product view

---
+++ djackets_vue/src/views/Product.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="page-product">
+    <div class="columns is-multilne">
+      <div class="column is-9">
+        <figure class="image mb-6">
+          <img :src="product.get_image" :alt="product.description">
+        </figure>
+        <h1 class="title">{{ product.name }}</h1>
+        <p>{{ product.description }}</p>
+      </div>
+      <div class="column is-3">
+        <h2 class="subtitle">Information</h2>
+        <p><strong>Price: </strong>${{ product.price }}</p>
+
+        <div class="field has-addons mt-6">
+          <div class="control">
+            <input class="input" type="number" min="1" v-model="quantity">
+          </div>
+
+          <div class="control">
+            <a class="button is-dark" href="">Add to cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  data() {
+    return {
+      product: {},
+      quantity: 1
+    }
+  },
+  mounted() {
+    this.getProduct()
+  },
+  methods: {
+    getProduct() {
+      const category_slug = this.$route.params.category_slug
+      const product_slug = this.$route.params.product_slug
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
