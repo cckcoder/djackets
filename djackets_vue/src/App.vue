6e975cf59e4a8306b2b33f3120546dffa58c40b1
codewizz <c.chaiwat.ch@gmail.com>
2021-04-14 08:26:43 +0700

    add: show cart quantity

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -25,7 +25,7 @@
               <router-link to="/log-in" class="button is-light">Log in</router-link>
               <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-                <span>Cart</span>
+                <span>Cart ({{ cartTotalLength }})</span>
               </router-link>
             </div>
           </div>
@@ -47,7 +47,23 @@
 export default {
   data() {
     return {
-      showMobileMenu: false
+      showMobileMenu: false,
+      cart: {
+        items: []
+      }
+    }
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+  },
+  computed: {
+    cartTotalLength() {
+      let totalLength = 0
+      for(let i=0; i<this.cart.items.length; i++) {
+        totalLength += this.cart.items[i].quantity
+      }
+
+      return totalLength
     }
   }
 }
