0ebfaebb9eadddc1f4a2249a47f41eef5ed6a800
codewizz <c.chaiwat.ch@gmail.com>
2021-04-14 10:24:59 +0700

    add: category view and url

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -25,3 +25,17 @@ class ProductDetail(APIView):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
