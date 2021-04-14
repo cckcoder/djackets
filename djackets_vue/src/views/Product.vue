6cc2f8fc11e63f1cb83f3db89c966a6cec5c016d
codewizz <c.chaiwat.ch@gmail.com>
2021-04-14 10:06:00 +0700

    add: implement loading bar

--- djackets_vue/src/views/Product.vue
+++ djackets_vue/src/views/Product.vue
@@ -42,11 +42,13 @@ export default {
     this.getProduct()
   },
   methods: {
-    getProduct() {
+    async getProduct() {
+      this.$store.commit('setIsLoading', true)
+
       const category_slug = this.$route.params.category_slug
       const product_slug = this.$route.params.product_slug
 
-      axios
+      await axios
         .get(`/api/v1/products/${category_slug}/${product_slug}`)
         .then(response => {
           this.product = response.data
@@ -54,6 +56,8 @@ export default {
         .catch(error => {
           console.log(error)
         })
+
+      this.$store.commit('setIsLoading', false)
     },
     addToCart() {
       if (isNaN(this.quantity) || this.quantity < 1) {
