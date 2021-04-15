e19134f96ecf58e5ecfde24bc17d1855c28e22fc
dev2 <c.chaiwat.ch@gmail.com>
2021-04-15 15:50:09 +0700

    add: component cart item

---
+++ djackets_vue/src/views/Cart.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="page-cart">
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h1 class="title">Cart</h1>
+      </div>
+      <div class="column is-12 box">
+        <table class="table is-fullwidth" v-if="cartTotalLength">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <CartItem
+              v-for="item in cart.items"
+              :key="item.product.id"
+              :initialItem="item"
+            />
+          </tbody>
+        </table>
+
+        <p v-else>You don't have any products in your cart...</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import CartItem from '@/components/CartItem'
+
+export default {
+  name: 'Cart',
+  components: {
+    CartItem
+  },
+  mounted() {
+    this.cart = this.$store.state.cart
+  },
+  data() {
+    return {
+      cart: {
+        items: []
+      }
+    }
+  },
+  computed: {
+    cartTotalLength() {
+      return this.cart.items.reduce((acc, curVal) => {
+        return acc += curVal.quantity
+      }, 0)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
