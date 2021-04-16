00451c72443a3c8aaa5af297d98cf6e231775a7d
dev2 <c.chaiwat.ch@gmail.com>
2021-04-16 13:36:29 +0700

    update: add form checkout

--- djackets_vue/src/views/CheckOut.vue
+++ djackets_vue/src/views/CheckOut.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page-checkout">
         <div class="columns is-multiline">
-            <div class="colum is-12">
+            <div class="column is-12">
                 <h1 class="title">Checkout</h1>
             </div>
 
@@ -15,10 +15,11 @@
                             <th>Total</th>
                         </tr>
                     </thead>
+
                     <tbody>
                         <tr
                             v-for="item in cart.items"
-                            :key="item.product.id"
+                            v-bind:key="item.product.id"
                         >
                             <td>{{ item.product.name }}</td>
                             <td>${{ item.product.price }}</td>
@@ -26,14 +27,90 @@
                             <td>${{ getItemTotal(item).toFixed(2) }}</td>
                         </tr>
                     </tbody>
+
                     <tfoot>
                         <tr>
                             <td colspan="2">Total</td>
                             <td>{{ cartTotalLength }}</td>
-                            <td>{{ cartTotalPrice.toFixed(2) }}</td>
+                            <td>${{ cartTotalPrice.toFixed(2) }}</td>
                         </tr>
                     </tfoot>
                 </table>
+            </div>
+
+            <div class="column is-12 box">
+                <h2 class="subtitle">Shipping details</h2>
+
+                <p class="has-text-danger mb-4">* All fields are required</p>
+
+                <div class="columns is-multiline">
+                    <div class="column is-6">
+                        <div class="field">
+                            <label>First name*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="first_name">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Last name*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="last_name">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>E-mail*</label>
+                            <div class="control">
+                                <input type="email" class="input" v-model="email">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Phone*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="phone">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="column is-6">
+                        <div class="field">
+                            <label>Address*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="address">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Zip code*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="zipcode">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Place*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="place">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="notification is-danger mt-4" v-if="errors.length">
+                    <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                </div>
+
+                <hr>
+
+                <div id="card-element" class="mb-5"></div>
+
+                <template v-if="cartTotalLength">
+                    <hr>
+
+                    <button class="button is-dark" @click="submitForm">Pay with Stripe</button>
+                </template>
             </div>
         </div>
     </div>
@@ -68,6 +145,42 @@ export default {
     methods: {
         getItemTotal(item) {
             return item.quantity * item.product.price
+        },
+        submitForm() {
+            this.errors = []
+            if (this.first_name === '') {
+                this.errors.push('The first name field is missing!')
+            }
+            if (this.last_name === '') {
+                this.errors.push('The last name field is missing!')
+            }
+            if (this.email === '') {
+                this.errors.push('The email field is missing!')
+            }
+            if (this.phone === '') {
+                this.errors.push('The phone field is missing!')
+            }
+            if (this.address === '') {
+                this.errors.push('The address field is missing!')
+            }
+            if (this.zipcode === '') {
+                this.errors.push('The zip code field is missing!')
+            }
+            if (this.place === '') {
+                this.errors.push('The place field is missing!')
+            }
+            if (!this.errors.length) {
+                this.$store.commit('setIsLoading', true)
+                this.stripe.createToken(this.card).then(result => {                    
+                    if (result.error) {
+                        this.$store.commit('setIsLoading', false)
+                        this.errors.push('Something went wrong with Stripe. Please try again')
+                        console.log(result.error.message)
+                    } else {
+                        this.stripeTokenHandler(result.token)
+                    }
+                })
+            }
         }
     },
     computed: {
