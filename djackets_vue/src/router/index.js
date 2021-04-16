6f1ce82cdc2919183b989abeb0a30be451104f0b
dev2 <c.chaiwat.ch@gmail.com>
2021-04-16 10:58:53 +0700

    add: sign-in

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -4,6 +4,7 @@ import Product from "../views/Product.vue";
 import Category from "../views/Category.vue";
 import Search from "../views/Search.vue";
 import Cart from "../views/Cart.vue";
+import SignUp from "../views/SignUp.vue";
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: "/search",
     name: "Search",
     component: Search,
+  },
+  {
+    path: "/sign-up",
+    name: "SignUp",
+    component: SignUp,
   },
   {
     path: "/cart",
