a9f780afbf577f0b8a22496c10aaa9b79c44cdd5
root <root@mail.bfgems.com>
2021-04-30 22:59:04 +0700

    update: setup deploy from production

--- djackets_django/djackets_django/asgi.py
+++ djackets_django/djackets_django/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djackets_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djackets_django.setting_production')
 
 application = get_asgi_application()
--- djackets_vue/src/main.js
+++ djackets_vue/src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.baseURL = "http://dapi.codewizz.org";
 
 createApp(App)
   .use(store)
